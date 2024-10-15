d03d6015600074eec5b9ad8c22e1e150888db14b
2021ec0450 <nishadharshinibnk@gmail.com>
2024-10-15 21:20:00 +0530

    initial commit

---
+++ models/Feedback.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+const feedbacksystem=new mongoose.Schema({
+    name: String,
+    contactNumber: String,
+    email:String,
+    feedback:String
+
+}
+   
+
+
+);
+module.exports=mongoos.model('feedback',feedbackSchema);
